d0317372cdb241ba2f9914a65444a4a25b82ea21
Ubiratan Tavares <ust1973@gmail.com>
2025-06-24 19:59:44 -0300

    atualização do arquivo app.py

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+import os
+from dotenv import load_dotenv
+
 from nf.zip_extractor import ZipExtractor
 from nf.csv_loader import CSVLoader
 from nf.formatter.nota_fiscal_preprocessor import NotaFiscalPreprocessor
 from nf.formatter.nota_fiscal_document_builder import NotaFiscalDocumentBuilder
 from nf.vectorstore import VectorStoreBuilder
 from nf.agent import RAGNotaFiscalAgent
+
+# Carregar variáveis do .env
+load_dotenv()
+
+# Recuperar a API Key da variável de ambiente
+api_key = os.getenv("OPENAI_API_KEY")
 
 # local onde encontra-se o arquivo zipado
 #zip_file_path = "./data/202401_NFs.zip"
@@ -43,16 +52,13 @@ vector_store = vs_builder.build_vectorstore(docs)
 # construindo e expondo o retriever baseado em FAISS
 retriever = vs_builder.get_retriever(vector_store)
 
-# 
-api_key = "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"
-
 agent = RAGNotaFiscalAgent(retriever, api_key)
 
-queries = ["Quais empresas emitiram notas fiscais acima de R$ 1000.000,00?",
-             "Qual a nota fiscal com maior valor emitido para o estado de SP?",
-             "Quais foram as três notas fiscais de maior valor emitidas em janeiro de 2024?",
-             "Quantas notas fiscais tiveram como destinatário o Fundo Nacional de Desenvolvimento da Educação?",
-             "Qual o valor total das notas fiscais destinadas ao estado de São Paulo (SP)?"]
+queries = ["Quais empresas emitiram notas fiscais acima de R$ 20.000,00?",
+           "Qual a nota fiscal com maior valor emitido para o estado de SP?",
+           "Quantas notas fiscais tiveram como destinatário o Fundo Nacional de Desenvolvimento da Educação?",
+           "Qual é o fornecedor que teve o maior valor recebido?",
+           "Qual a descrição do item com o maior valor na quantidade entre todos as notas fiscais apresentadas?"]
              
 for query in queries:
     print(f"\nPergunta: {query}")
